a79b84ecfd059dd8797cd1af4ec80e93ec33651d
Melan <melan2308@gmail.com>
2018-07-23 21:40:39 +0200

    Perform transformations using transforms.Compose on train, test & validation data

--- LeNet/PyTorch/main.py
+++ LeNet/PyTorch/main.py
@@ -1,5 +1,5 @@
 import json
-from data_loader import CifarDataLoader, CifarDataset, ToTensor
+from data_loader import CifarDataLoader, CifarDataset, ToTensor, ToGrayscale, Normalize
 
 import torch
 from torch.utils.data import DataLoader 
@@ -9,17 +9,27 @@ def main():
 	with open("config.json", "r") as f:
 		config = json.load(f)
 
-	data = CifarDataLoader(config)
+	data = CifarDataset(config)
 
-	train_data_transform = transforms.Compose([ToTensor()])
-	train_data_transformed = CifarDataset(config, data.X_train, data.y_train, 
-		transform=train_data_transform)
+	all_transforms = transforms.Compose([
+		ToGrayscale(), 
+		Normalize(), 
+		ToTensor()])
+
+	train_data_transformed = CifarDataLoader(config, data.X_train, data.y_train, 
+		transform=all_transforms)
+	valid_data_transformed = CifarDataLoader(config, data.X_valid, data.y_valid, 
+		transform=all_transforms)
+	test_data_transformed = CifarDataLoader(config, data.X_test, data.y_test, 
+		transform=all_transforms)
 
 	# print("data.X_train[9].shape: ", data.X_train[9].shape)
 
 	for i in range(4): 
 		sample = train_data_transformed[i]
 		print(i, sample['image'].size(), sample['label'].size())
+
+	
 
 
 if __name__ == '__main__':
