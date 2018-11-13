6c1278897a36164cc57e6d7abe8fa5409219ee1b
Melan <melan2308@gmail.com>
2018-11-13 21:36:18 +0100

    Define function data train for estimator in train mode

--- AlexNet/TensorFlow/main.py
+++ AlexNet/TensorFlow/main.py
@@ -15,19 +15,10 @@ def main():
 	# Load Cifar data 
 	data = CifarDataset(config)
 
-	(X_train, y_train) = data.X_train, data.y_train 
-	(X_test, y_test) = data.X_test, data.y_test
 
-	train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train)).batch(32)
 
-	# X_train = tf.data.Dataset.from_tensor_slices(X_train)
-	# y_train = tf.data.Dataset.from_tensor_slices(y_train)
-	# Rescale images
-	train_dataset = train_dataset.map(lambda x, y: (tf.div(tf.cast(x, tf.float32), 255.0), y))
-	# Resize images 
-	train_dataset = train.dataset.map()
-	# One hot 
-	train_dataset = train_dataset.map(lambda x, y: (x, tf.one_hot(y, 10)))
+
+	x, y = train_input_fn(config, data)
 
 	pdb.set_trace()
 
@@ -40,6 +31,46 @@ def main():
 
 	# # Create AlexNet model 
 	# net = AlexNet(config)
+
+
+
+def train_input_fn(config, data):
+	# Extract train data
+	X_train, y_train = data.X_train, data.y_train
+	# Create tf.data.Dataset instance from numpy array
+	train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+	# Rescale images
+	train_dataset = train_dataset.map(lambda x, y: (tf.div(tf.cast(x, tf.float32), 255.0), y))	
+	# Resize images 
+	train_dataset = train_dataset.map(lambda x, y: (tf.image.resize_images(x, [256, 256]), y))
+
+	# TODO : Add image augmentation functions
+	
+	# One hot 
+	train_dataset = train_dataset.map(lambda x, y: (x, tf.one_hot(y, 10)))
+	# Read a buffer and randomly shuffle it
+	train_dataset = train_dataset.shuffle(buffer_size=config["trainer"]["buffer_size"])
+	# Allow infinite reading of the data in training 
+	num_repeat = None 
+	train_dataset = train_dataset.repeat(count=num_repeat)
+	# Get a batch of the given size
+	train_dataset = train_dataset.batch(config["trainer"]["batch_size"])
+	# Create an iterator for the dataset and the above modifications
+	iterator = train_dataset.make_one_shot_iterator()
+	# Get the next batch of images and labels
+	images_batch, labels_batch = iterator.get_next()
+
+	# The input-function must return a dict wrapping the images
+	x = {'image': images_batch}
+	y = labels_batch 
+
+	return x, y
+
+
+def train_input_fn_wrapper():
+	""" Wrap the train_input_fn because the estimator function does not accept
+	any input arguments """
+	return train_input_fn(config=config)
 
 
 if __name__ == '__main__':
