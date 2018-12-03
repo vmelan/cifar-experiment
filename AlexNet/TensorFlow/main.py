dd974122f2d4361c79cd59ebc9030b9ba736c2c7
Melan <melan2308@gmail.com>
2018-12-03 22:18:57 +0100

    Create wrapper for training data without use of global variables

--- AlexNet/TensorFlow/main.py
+++ AlexNet/TensorFlow/main.py
@@ -20,7 +20,7 @@ def main():
 
 
 
-	x, y = train_input_fn(config, data)
+	# x, y = train_input_fn(config, data)
 
 	# pdb.set_trace()
 
@@ -33,8 +33,11 @@ def main():
 	## Training 
 	# model.train(input_fn=train_input_fn, 
 	# 	steps=config["trainer"]["epochs"] * config["trainer"]["batch_size"])
+	train_input_fn_wrapper_estimator = train_input_fn_wrapper(config=config, data=data)
 
-	model.train(input_fn=train_input_fn_wrapper, 
+	pdb.set_trace()
+
+	model.train(input_fn=train_input_fn_wrapper_estimator, 
 		steps=config["trainer"]["epochs"] * config["trainer"]["batch_size"])
 
 	# Test
@@ -45,50 +48,89 @@ def main():
 
 
 
-def train_input_fn(config, data):
-	# Extract train data
-	X_train, y_train = data.X_train, data.y_train
-	# Create tf.data.Dataset instance from numpy array
-	train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
-	# Rescale images
-	train_dataset = train_dataset.map(lambda x, y: (tf.div(tf.cast(x, tf.float32), 255.0), y))	
-	# Resize images 
-	train_dataset = train_dataset.map(lambda x, y: (tf.image.resize_images(x, [256, 256]), y))
+# def train_input_fn(config, data):
+# 	# Extract train data
+# 	X_train, y_train = data.X_train, data.y_train
+# 	# Create tf.data.Dataset instance from numpy array
+# 	train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+# 	# Rescale images
+# 	train_dataset = train_dataset.map(lambda x, y: (tf.div(tf.cast(x, tf.float32), 255.0), y))	
+# 	# Resize images 
+# 	train_dataset = train_dataset.map(lambda x, y: (tf.image.resize_images(x, [256, 256]), y))
 
-	# TODO : Add image augmentation functions
+# 	# TODO : Add image augmentation functions
 	
-	# One hot 
-	train_dataset = train_dataset.map(lambda x, y: (x, tf.one_hot(y, 10)))
-	# Read a buffer and randomly shuffle it
-	train_dataset = train_dataset.shuffle(buffer_size=config["trainer"]["buffer_size"])
-	# Allow infinite reading of the data in training 
-	num_repeat = None 
-	train_dataset = train_dataset.repeat(count=num_repeat)
-	# Get a batch of the given size
-	train_dataset = train_dataset.batch(config["trainer"]["batch_size"])
-	# Create an iterator for the dataset and the above modifications
-	iterator = train_dataset.make_one_shot_iterator()
-	# Get the next batch of images and labels
-	images_batch, labels_batch = iterator.get_next()
+# 	# One hot 
+# 	train_dataset = train_dataset.map(lambda x, y: (x, tf.one_hot(y, 10)))
+# 	# Read a buffer and randomly shuffle it
+# 	train_dataset = train_dataset.shuffle(buffer_size=config["trainer"]["buffer_size"])
+# 	# Allow infinite reading of the data in training 
+# 	num_repeat = None 
+# 	train_dataset = train_dataset.repeat(count=num_repeat)
+# 	# Get a batch of the given size
+# 	train_dataset = train_dataset.batch(config["trainer"]["batch_size"])
+# 	# Create an iterator for the dataset and the above modifications
+# 	iterator = train_dataset.make_one_shot_iterator()
+# 	# Get the next batch of images and labels
+# 	images_batch, labels_batch = iterator.get_next()
 
-	# The input-function must return a dict wrapping the images
-	x = {'image': images_batch}
-	y = labels_batch 
+# 	# The input-function must return a dict wrapping the images
+# 	x = {'image': images_batch}
+# 	y = labels_batch 
 
-	return (x, y)
+# 	return (x, y)
 
 
 
-with open("config.json", "r") as f:
-	config = json.load(f)
+# with open("config.json", "r") as f:
+# 	config = json.load(f)
 
-# Load Cifar data 
-data = CifarDataset(config)
+# # Load Cifar data 
+# data = CifarDataset(config)
 
-def train_input_fn_wrapper():
+# def train_input_fn_wrapper():
+# 	""" Wrap the train_input_fn because the estimator function does not accept
+# 	any input arguments """
+# 	return train_input_fn(config=config, data=data)
+
+
+
+def train_input_fn_wrapper(config, data):
+	def train_input_fn():
+		# Extract train data
+		X_train, y_train = data.X_train, data.y_train
+		# Create tf.data.Dataset instance from numpy array
+		train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+		# Rescale images
+		train_dataset = train_dataset.map(lambda x, y: (tf.div(tf.cast(x, tf.float32), 255.0), y))	
+		# Resize images 
+		train_dataset = train_dataset.map(lambda x, y: (tf.image.resize_images(x, [256, 256]), y))
+
+		# TODO : Add image augmentation functions
+		
+		# One hot 
+		train_dataset = train_dataset.map(lambda x, y: (x, tf.one_hot(y, 10)))
+		# Read a buffer and randomly shuffle it
+		train_dataset = train_dataset.shuffle(buffer_size=config["trainer"]["buffer_size"])
+		# Allow infinite reading of the data in training 
+		num_repeat = None 
+		train_dataset = train_dataset.repeat(count=num_repeat)
+		# Get a batch of the given size
+		train_dataset = train_dataset.batch(config["trainer"]["batch_size"])
+		# Create an iterator for the dataset and the above modifications
+		iterator = train_dataset.make_one_shot_iterator()
+		# Get the next batch of images and labels
+		images_batch, labels_batch = iterator.get_next()
+
+		# The input-function must return a dict wrapping the images
+		x = {'image': images_batch}
+		y = labels_batch 
+
+		return (x, y)		
+
 	""" Wrap the train_input_fn because the estimator function does not accept
 	any input arguments """
-	return train_input_fn(config=config, data=data)
+	return train_input_fn # return function call 
 
 
 
